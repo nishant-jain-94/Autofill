b2f5f2ab41cbbb9c8d7c75fb704dde69605acfcc
Ganesh Patel <ganeshpatel1618@gmil.com>
2017-05-12 18:01:32 +0530

    pos model updated..

--- notebooks/get_POS_tags.py
+++ notebooks/get_POS_tags.py
@@ -1,7 +1,19 @@
+
+# coding: utf-8
+
+# In[9]:
+
 import spacy
 import re
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+
+# In[10]:
 
 nlp = spacy.load('en')
+
+
+# In[14]:
 
 def find_POS(raw_text_list):
     final_pos_sents = []
@@ -9,7 +21,7 @@ def find_POS(raw_text_list):
         doc = nlp(sent)
         pos = []
         for word in doc:
-            pos.append((word, word.pos_))
+            pos.append(word.pos_)
         final_pos_sents.append(pos)
     return final_pos_sents 
 
