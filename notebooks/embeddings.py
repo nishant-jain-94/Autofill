de4e8edc578269be870a723510b5f3fc7f98b1d3
Subham <subham@gmail.com>
2017-05-15 09:52:45 +0530

    modified embeddings and pos file

--- notebooks/embeddings.py
+++ notebooks/embeddings.py
@@ -1,19 +1,21 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 import re
 import os
 import numpy as np
 import json
 import pickle
+import datetime
 from nltk.tokenize import word_tokenize,sent_tokenize
 from gensim.models import Word2Vec
 from load_squad_wiki_data import get_squad_wiki_data
+from get_POS_tags import find_POS
 
 
-# In[2]:
+# In[ ]:
 
 class Embeddings:
     def __init__(self, size, window, min_count, workers):
@@ -26,16 +28,34 @@ class Embeddings:
         self.path_indexed_sentences = '../data/indexed_sentences_{0}.json'.format(base_file_name)
         self.path_word_embeddings = '../data/word_embeddings_{0}.npz'.format(base_file_name)
         self.path_indexed_vocabulary = '../data/indexed_vocabulary_{0}.json'.format(base_file_name)
+        self.path_pos_indexed_sentences = '../data/pos_indexed_sentences_{0}.json'.format(base_file_name)
+        self.path_pos_indexed_vocabulary = '../data/pos_indexed_vocabulary_{0}.json'.format(base_file_name)
         self.load_embeddings()
         
     def preprocessor(self, raw_text, size, window, min_count, workers):
         # removes all the punctuations and retains only alphabets,digits,fullstop,apostrophe
-        clean_text = re.sub(r'[^\w\'\.\+\-\=\*\^]',' ' ,raw_text)
+        clean_text = re.sub(r'[^\w\'\.\+\-\=\*\s\^]','' ,raw_text)
         # sentence tokenize clean text
         sentences = sent_tokenize(clean_text)
         # replace full stops from each sentence in sentences list by null
         sentences = [sent.replace('.','') for sent in sentences]
-        # word tokenize each sentence in sentences list
+        # finding tokenized pos sentences
+        tokenized_pos_sentences = find_POS(sentences)
+        # finding out the vocabulary of pos tagged sentences
+        vocab = []
+        for sent in tokenized_pos_sentences:
+                vocab.extend(sent) 
+        vocab = sorted(list(set(vocab)))
+        vocab = dict((word, index) for index, word in enumerate(vocab))
+        # Storeing the vocab2index of pos tags in a seperate file
+        with open(self.path_pos_indexed_vocabulary,'w') as outfile:
+            json.dump(vocab,outfile)
+        # replacing each word of tokenized_pos_sentences by coressponding vocabulary index
+        indexed_pos_sentences = [[vocab[word] for word in sent] for sent in tokenized_pos_sentences]
+        # storeing indexed_pos_sentences(tokenized pos sentences) 
+        with open(self.path_pos_indexed_sentences,'w') as outfile:
+            json.dump(indexed_pos_sentences,outfile)
+        # word tokenize each sentence in sentences list 
         tokenized_sentences = [word_tokenize(sent) for sent in sentences]
         # initialize word2vector model
         model = Word2Vec(sentences = tokenized_sentences, size = size, window = window, min_count = min_count, workers = workers)
@@ -99,35 +119,53 @@ class Embeddings:
     def get_tokenized_indexed_sentences(self):
         with open(self.path_indexed_sentences, 'r') as f:
             tokenized_sentences = json.load(f)
-        return tokenized_sentences    
-
-
-# In[3]:
-
-e = Embeddings(5,4,1,4)
-
-
-# In[4]:
-
-e.get_model()
-
-
-# In[5]:
-
-e.get_weights()
-
-
-# In[76]:
-
-e.get_vocabulary()
-
-
-# In[77]:
-
-e.get_tokenized_indexed_sentences()
+        return tokenized_sentences
+    
+    # Returns pos2Index and index2pos
+    def get_pos_vocabulary(self):
+        with open(self.path_pos_indexed_vocabulary, 'r') as f:
+            data = json.load(f)
+        pos2idx = data
+        idx2pos = dict([(v, k) for k, v in data.items()])
+        return pos2idx, idx2pos
+    
+    # Returns the tokenized pos sentences
+    def get_pos_tokenized_indexed_sentences(self):
+        with open(self.path_pos_indexed_sentences, 'r') as f:
+            tokenized_pos_sentences = json.load(f)
+        return tokenized_pos_sentences
 
 
 # In[ ]:
 
+start_date = datetime.datetime.now()
+e = Embeddings(5,4,1,4)
+end_date = datetime.datetime.now()
+print("TOTAL TIME ELAPSED IN EMBEDDINGS.PYNB")
+print(((end_date - start_date).hour)," HOURS ",((end_date - start_date).minute)," MINUTES ",((end_date - start_date).second)," SECONDS ")
 
+
+# In[ ]:
+
+#e.get_model()
+
+
+# In[ ]:
+
+#e.get_weights()
+
+
+# In[ ]:
+
+#e.get_vocabulary()
+
+
+# In[ ]:
+
+#e.get_tokenized_indexed_sentences()
+
+
+# In[ ]:
+
+#vocab = ['PUNCT','SYM','X','ADJ','VERB','CONJ','NUM','DET','ADV','PROPN','NOUN','PART','INTJ','CCONJ','','']
 
