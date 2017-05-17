28e382022b8f7403027abbea0443f8c2d4a5b1ec
harshel <jain.harshel@gmail.com>
2017-05-17 18:53:10 +0530

    file added

--- notebooks/POS_GRU_MSE_RELU_5_1024_512.py
+++ notebooks/POS_GRU_MSE_RELU_5_1024_512.py
@@ -12,7 +12,7 @@ import json
 
 from keras.utils import to_categorical
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
+from keras.layers import Dense, Dropout, GRU
 from keras.callbacks import ModelCheckpoint
 from embeddings import Embeddings
 
