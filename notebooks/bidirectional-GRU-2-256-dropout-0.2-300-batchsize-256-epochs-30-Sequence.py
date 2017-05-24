5d2b5df718b2de1ab67c6dc31a6ac1213226e7b6
Nishant Jain <nishantkumarajain@gmail.com>
2017-05-24 09:18:08 +0530

    Added Model Summary in Bidirectional GRU

--- notebooks/bidirectional-GRU-2-256-dropout-0.2-300-batchsize-256-epochs-30-Sequence.py
+++ notebooks/bidirectional-GRU-2-256-dropout-0.2-300-batchsize-256-epochs-30-Sequence.py
@@ -1,9 +1,9 @@
 
 # coding: utf-8
 
-# ### importing require packages
+# ## Importing required packages
 
-# In[1]:
+# In[ ]:
 
 from __future__ import print_function
 
@@ -29,16 +29,32 @@ import random
 from itertools import groupby
 
 
+# ## Setting Parameters
+
+# In[ ]:
+
+model_name = "bidirectional-GRU-2-256-dropout-0.2-300-batchsize-256-epochs-30-Sequence"
+word_embedding_dimension = 300
+word_embedding_window_size = 4
+batch_size = 256 # 32, 64, 128
+epochs = 30 # 10, 15, 30
+window_size = "None" # 3, 4, 5
+accuracy_threshold = 1
+activation = 'softmax' # sigmoid, relu, softmax
+custom_accuracy = 0
+loss_function = 'cosine_proximity' # mse
+
+
 # ## Instantiate Embeddings 
 
-# In[2]:
+# In[ ]:
 
 embeddings = Embeddings(300, 4, 1, 4)
 
 
 # ### Getting data from preprocessing
 
-# In[3]:
+# In[ ]:
 
 word2vec_model = embeddings.get_intersected_model()
 word2index, index2word = embeddings.get_vocabulary()
@@ -46,62 +62,37 @@ word2vec_weights = word2vec_model.wv.syn0
 tokenized_indexed_sentences = embeddings.get_indexed_sentences()
 
 
-# In[4]:
+# In[ ]:
 
 word2index = {word:index+1 for word, index in word2index.items()}
 index2word = {index:word for word, index in word2index.items()}
 
 
-# In[5]:
-
-word2index
-
-
-# In[6]:
-
-tokenized_indexed_sentences[0]
-
-
-# In[7]:
+# In[ ]:
 
 tokenized_indexed_sentences = [np.array(sentence) + 1 for sentence in tokenized_indexed_sentences if len(sentence) > 0]
 
 
-# In[8]:
-
-tokenized_indexed_sentences[0]
-
-
-# In[9]:
+# In[ ]:
 
 new_weights = np.zeros((1, word2vec_weights.shape[1]))
 
 
-# In[10]:
+# In[ ]:
 
 new_weights = np.append(new_weights, word2vec_weights, axis=0)
 
 
-# In[11]:
+# ## Generating Training Data
 
-new_weights.shape
-
-
-# In[12]:
-
-new_weights[52730]
-
-
-# ### generating training data
-
-# In[13]:
+# In[ ]:
 
 window_size = 5
 vocab_size = len(word2index)
 print(vocab_size)
 
 
-# In[14]:
+# In[ ]:
 
 maxlen = max([len(sentence) for sentence in tokenized_indexed_sentences])
 
@@ -113,26 +104,22 @@ tokenized_indexed_sentences = sequence.pad_sequences(tokenized_indexed_sentences
 
 # In[ ]:
 
-seq_in = []
-seq_out = []
-# generating dataset
-tokenized_indexed_sentences = [sentence for sentence in tokenized_indexed_sentences if len(sentence) > 0]
-for sentence in tokenized_indexed_sentences:
-    x = sentence
-    y = np.append(sentence[1:], np.array(sentence[len(sentence)-1]))
-    seq_in.append(x)
-    seq_out.append([new_weights[index] for index in y])
+seq_in = np.zeros_like(tokenized_indexed_sentences)
+seq_out = np.zeros((tokenized_indexed_sentences.shape[0], tokenized_indexed_sentences.shape[1], 300))
 
-# converting seq_in and seq_out into numpy array
-seq_in = np.array(seq_in)
-seq_out = np.array(seq_out)
+# Generating Dataset
+for index, sentence in enumerate(tokenized_indexed_sentences):
+    y = np.append(sentence[1:], np.array(sentence[len(sentence)-1]))
+    seq_in[index] += sentence
+    seq_out[index] += [new_weights[i] for i in y]
+
 n_samples = len(seq_in)
 print ("Number of samples : ", n_samples)
 
 
 # ## Defining model
 
-# In[17]:
+# In[ ]:
 
 # Changes to the model to be done here
 model = Sequential()
@@ -140,13 +127,15 @@ model.add(Embedding(input_dim=new_weights.shape[0], output_dim=new_weights.shape
 model.add(Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))
 model.add(Dropout(0.2))
 model.add(Bidirectional(GRU(300, return_sequences=True), merge_mode="ave"))
-model.compile(loss='cosine_proximity', optimizer='adam',metrics=['accuracy'])
+model.compile(loss=loss_function, optimizer='adam',metrics=['accuracy'])
 model.summary()
 
 
-# In[18]:
+# ## Creating Weights Directory
 
-model_weights_path = "../weights/bidirectional-GRU-2-256-dropout-0.2-300-batchsize-256-epochs-30-Sequence"
+# In[ ]:
+
+model_weights_path = "../weights/" + model_name
 if not os.path.exists(model_weights_path):
     os.makedirs(model_weights_path)
 checkpoint_path = model_weights_path + '/weights.{epoch:02d}.hdf5'
@@ -155,116 +144,60 @@ checkpoint = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_best_only
 
 # ## Train Model
 
-# In[20]:
+# In[ ]:
 
-model.fit(seq_in, seq_out, epochs=30, verbose=30, batch_size=256, callbacks=[checkpoint])
+model_fit_summary = model.fit(seq_in, seq_out, epochs=epochs, verbose=1, batch_size=batch_size, callbacks=[checkpoint])
 
 
-# ### model predict
+# ## Predictions
 
-# In[45]:
+# In[ ]:
 
 start = 0
-sentence_test = " who is the"
+sentence_test = "In which regions in particular did"
 indexed_sentences = embeddings.get_indexed_query(sentence_test)
 print("indexed_sentences ",indexed_sentences)
-sent = np.array(indexed_sentences) + 1
+sent = np.array(indexed_sentences)
 #pattern = list(seq_in[start])
 pattern = list(sent)
 print("\"",' '.join(index2word[index] for index in pattern))
-for i in range(2):
+for i in range(10):
     prediction = model.predict(np.array([pattern]))
-    print(len(prediction[0]))
     pred_word = word2vec_model.similar_by_vector(prediction[0][prediction.shape[1] - 1])[0][0]
     sys.stdout.write(pred_word+" ")
     pattern.append(word2index[pred_word])
     pattern = pattern[:len(pattern)]
 
 
-# In[34]:
-
-model.layers[1].get_weights()[0]
-
-
-# In[37]:
-
-len(tokenized_indexed_sentences)
-
+# ## Model Summary
 
 # In[ ]:
 
-#e_model = embeddings.get_model()
-
-
-# In[ ]:
-
-#e_model.similar_by_word("profitabl")
-
-
-# ## Accuracy
-
-# In[ ]:
-
-def accuracy():
-    count = 0
-    correct = 0
-    for sub_sample_in, sub_sample_out in zip(seq_in, seq_out):
-        ypred = model.predict_on_batch(np.expand_dims(sub_sample_in, axis=0))[0]
-        ytrue = sub_sample_out
-        pred_word = word2vec_model.similar_by_vector(ypred)[0][0]
-        true_word = word2vec_model.similar_by_vector(ytrue)[0][0]
-        similarity = word2vec_model.similarity(pred_word, true_word)
-        if similarity == 1:
-            correct += 1
-        count += 1
-    print("Accuracy {0}".format(correct/count))
-
-
-# In[ ]:
-
-#seq_out[0]
-
-
-# In[ ]:
-
-# accuracy()
-
-
-# In[ ]:
-
-#model_results = model_fit_summary.history
-
-
-# In[ ]:
-
-#model_results.update(model_fit_summary.params)
-
-
-# In[ ]:
-
-#model_results["train_accuracy"] = accuracy()
-
-
-# In[ ]:
-
-# n = no. of predictions
-# accuracy = accuracy(400)
-#print(model_results)
-
-
-# In[ ]:
-
-#text_file_path = "../weights/lstm-2-1024-512-batchsize-128-epochs-25/model_results.json"
-
-
-# In[ ]:
-
-#with open(text_file_path, "w") as f:
-        #json.dump(model_results, f)
-        
-
-
-# In[ ]:
-
-
+model_results = model_fit_summary.history
+model_results.update(model_fit_summary.params)
+model_results["word_embedding_dimension"] = word_embedding_dimension
+model_results["word_embedding_window_size"] = word_embedding_window_size
+model_results["window_size"] = window_size
+model_results["batch_size"] = batch_size
+model_results["epochs"] = epochs
+model_results["model_name"] = model_name
+model_results["accuracy_threshold"] = accuracy_threshold
+model_results["activation"] = activation 
+model_results["custom_accuracy"] = custom_accuracy
+model_results["loss_function"] = loss_function
+model_results["layers"] = []
+model_results["dropouts"] = []
+for layer in model.layers:
+    if hasattr(layer, "units"):
+        layer_summary = {}
+        layer_summary["units"] = layer.get_config()["units"]
+        layer_summary["name"] = layer.name
+        model_results["layers"].append(layer_summary)
+    if hasattr(layer, "rate"):
+        dropout_summary = {}
+        dropout_summary["rate"] = layer.get_config()["rate"]
+        model_results["dropouts"].append(dropout_summary)
+text_file_path = "../weights/{0}/model_results.json".format(model_name)
+with open(text_file_path, "w") as f:
+        json.dump(model_results, f)
 
